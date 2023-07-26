e37ade57fc3b3337a1240753578b7407cafc00f7
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-07-26 12:47:28 +0300

    pos

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,13 +1,13 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+let counter = 0;
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
-
-let counter = 0;
 
 function updateCounter() {
   counter++;
