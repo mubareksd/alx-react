e28e15d59cb0b1b4a8b443db9c19a59f170511ef
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-07-26 22:07:50 +0300

    solution to task 8

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-function getFullYear() {
-  return new Date().getFullYear();
-}
-
-function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
-
-export { getFullYear, getFooterCopy };
